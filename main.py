041042839bea4bec3bb48e140a246732c585c6aa
SebaCen <sebastian.centurion@gmail.com>
2023-06-09 11:56:52 -0300

    cambio para que devuelva json

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ movies_wc['release_date'] = pd.to_datetime(movies_wc['release_date'])
 
 @app.get('/')
 def message():
-    return "Hello World"
+    return "Proyecto Integrador sobre Recomendacion de Peliculas"
 
 @app.get('/mes/{mes}')
 def cantidad_filmaciones_mes(mes: str):
@@ -23,9 +23,11 @@ def cantidad_filmaciones_mes(mes: str):
         mes_numero = meses.index(mes.lower()) + 1
         filas_con_mes = movies_wc[movies_wc['release_date'].dt.month == mes_numero]
         cant_meses = len(filas_con_mes)
-        return (f"{cant_meses} cantidad de películas fueron estrenadas en el mes de {mes}")
+        #return (f"{cant_meses} cantidad de películas fueron estrenadas en el mes de {mes}") 
+        return {'mes':mes, 'cantidad':cant_meses}
+
     except ValueError:
-        return ("Error al ingresar el mes, ingrese mes por su nombre por ejemplo: enero ")
+        return {'Error': 'ingrese el mes por su nombre, por ejemplo: enero'}
     
 dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
 
@@ -36,8 +38,9 @@ def cantidad_filmaciones_dia(dia: str):
         dia_numero = dias.index(dia.lower()) + 1
         filas_con_dias = movies_wc[movies_wc['release_date'].dt.dayofweek == dia_numero]
         cant_dias = len(filas_con_dias)
-        return (f'{cant_dias} cantidad de películas fueron estrenadas en los días {dia}')
+        return {'dia':dia, 'cantidad':cant_dias}
+    #return (f'{cant_dias} cantidad de películas fueron estrenadas en los días {dia}')
     except ValueError:
-        return ("Error al ingresar el dia, ingrese el dia nuevamente, evite usar acentos")
+        return {'Error':'Ingrese el dia nuevamente, evite usar acentos. Ejemplo: lunes'}
 
 print(cantidad_filmaciones_dia('jueves'))
